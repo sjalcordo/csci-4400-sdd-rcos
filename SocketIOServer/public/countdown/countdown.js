246606ad61ea1a5691d4268d61aef8c0f9bdf1a3
Amanda Ruan <amandaruan262@gmail.com>
2024-11-01 02:06:55 -0400

    Create Countdown Page + Animation
    Implemented the animation for the countdown page – the hearts fill up one by one and then the number decreases. After all the hearts for "1" are filled, it displays Game Start.

---
+++ SocketIOServer/public/countdown/countdown.js
@@ -0,0 +1,35 @@
+// "loading" the hearts - change the image after 0.5 seconds
+const number = document.querySelector('h1');
+const images = document.querySelectorAll('img');
+const container = document.getElementById('container');
+let current = Number(number.innerHTML);
+
+function animate (count) {
+
+    // update images: empty hearts to filled hearts
+    images.forEach((image, index) => {
+        setTimeout(() => {
+            image.src = "../Resources/heart-filled.png";
+        }, 500 * (index + 1));
+    });
+
+    // wait until above animation is done before changing num and resetting
+    setTimeout(() => {
+        // decrease count and update html
+        current = count - 1;
+        if (count == 1) {   // stop after filling in hearts for 1
+            container.innerHTML="<h1>GAME START!</h1>";
+            return;
+        }
+        number.innerHTML = current;
+
+        // reset images to empty hearts
+        images.forEach((image) => {
+            image.src = "../Resources/heart-outline.png";
+        })
+
+        animate(current);
+    }, 2000);
+};
+
+animate(current);
