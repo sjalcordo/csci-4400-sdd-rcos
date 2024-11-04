f623b44a794e224fa259d50381b1d061f33b31f8
Michael <yeyun163@gmail.com>
2024-11-04 00:00:46 -0500

    added progress page with player prefabs

---
+++ RCOS/Assets/Scripts/ProgressBarHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarHandler : MonoBehaviour
+{
+    private Slider slider;
+    private ParticleSystem particles;
+    public GameObject particleChild;
+    public float FillSpeed = 0.5f;
+    public float fillTarget = 0.0f;
+    private float targetProgress = 0;
+
+    private void Awake() {
+        slider = gameObject.GetComponent<Slider>();
+        particles = particleChild.GetComponent<ParticleSystem>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        setProgress(fillTarget);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value < targetProgress) {
+           slider.value += FillSpeed * Time.deltaTime;  
+           if (!particles.isPlaying)
+            particles.Play();
+        }
+        else {
+            particles.Stop();
+        }
+ 
+    }
+
+    //Add incremental progress to the bar until targetProgress
+    public void setProgress(float newProgress) {
+        targetProgress = slider.value + newProgress;
+    }
+}
